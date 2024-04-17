af296aa2c649079b712feea0a5e9226f70c6dde8
tatianaanosova <anosova@hypeauditor.com>
2024-04-17 13:54:23 +0200

    add colors, fonts, layouts

---
+++ src/components/basic/Button.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'white',
+    validator: (value) => ['white', 'blue'].includes(value),
+  },
+});
+
+</script>
+
+<template>
+  <button :class="'button button-' + props.type">
+    <span class="icon" v-if="$slots.icon"><slot name="icon"></slot></span>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.button {
+    display: flex;
+    align-items: center;
+    padding: 10px 50px;
+    border-radius: 5px;
+    font-size: 16px;
+}
+.button-white {
+    color: var(--blue-500);
+    border: 2px solid var(--gray-30);
+}
+.button-blue {
+    background-color: var(--blue-500);
+    color: var(--white);
+}
+.icon {
+    margin-top: 5px;
+    margin-right: 5px;
+}
+</style>
---
+++ src/components/layout/Content.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+<main class="content">
+    <slot></slot>  
+</main>
+</template>
+
+
+<style scoped>
+.content {
+    width: 1280px;
+    margin: 0 auto;
+}
+</style>
